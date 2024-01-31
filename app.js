3255f3ec2fec54d54b64552cd918ed83983441be
Mina Magdy <mina.magdy.takawey@gmail.com>
2024-01-31 20:36:38 +0200

    CORS error

--- app.js
+++ app.js
@@ -11,13 +11,17 @@ const predictionRouter = require('./routers/predictionRoutes');
 const app = express();
 
 // Middlewares
-app.use(cors());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-
+app.use(function (req, res, next) {
+    // res.header("Access-Control-Allow-Origin", "*");
+    const allowedOrigins = ['http://localhost:5173'];
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        console.log("allowedOrigins included origin", origin)
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
     next();
 });
 app.use(express.json());
