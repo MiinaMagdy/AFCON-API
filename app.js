2412c26dba8a99752ad2a47edd9ba23af944cd9a
Mina Magdy <mina.magdy.takawey@gmail.com>
2024-01-31 18:55:20 +0200

    Access control allow origin

--- app.js
+++ app.js
@@ -13,6 +13,12 @@ const app = express();
 // Middlewares
 app.use(express.json());
 app.use(cors({ origin: '*' }));
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 // Routers
 app.use('/api/v1/predictions', predictionRouter);
